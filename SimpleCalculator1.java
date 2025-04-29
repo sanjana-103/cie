ce1302974adc6980d0a05d76be52836f01afe063
sanjana-103 <sanjanamulagund03@gmail.com>
2025-04-29 03:24:46 -0700

    Update SimpleCalculator1.java

--- SimpleCalculator1.java
+++ SimpleCalculator1.java
@@ -1,5 +1,5 @@
 public class SimpleCalculator1 {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         int a = 10;
         int b = 5;
         char op = '+';
